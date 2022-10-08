176e383d3c3bdababaf04cfe66dd843a250b4738
OLATOUNDE Aymar <dolatoundel@gmail.com>
2022-10-07 23:18:19 -0400

    Update merge-two-sorted-linked-lists.cpp

--- merge-two-sorted-linked-lists.cpp
+++ merge-two-sorted-linked-lists.cpp
@@ -1,3 +1,16 @@
+/**
+ * Generate merge list from two sorted list. The method, here, is insert the
+ * first list element is second list. Suppose List 1 [a1, a2, a2, ..., an] 
+ * (a1 < a2 < ... <an) [1] and list 2 [b1, b2, b3, ..., bn] (b1 < b2 < b3 < ... < bn).
+ * If I insert  a1 in list 2 maintaining the order of the list 2 and I have
+ * [b1, b2, a1, b3, ..., bn], we'll have b2 < a1 < b3 [2]. According to [1] 
+ * and [2] (a1 < a2 and b2 < a1 < b3), we have b2 < a2 and a2 will be inserted 
+ * after b2.
+ * 
+ * @param head1 
+ * @param head2 
+ * @return SinglyLinkedListNode* 
+ */
 SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
     SinglyLinkedListNode* after_head1 = nullptr;
     
